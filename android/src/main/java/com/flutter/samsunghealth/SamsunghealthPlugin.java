a6fc91d5d2da12307e5b13932438889f7e956bb2
mondoktamas <mondok.tamas1@gmail.com>
2020-04-16 10:08:49 +0300

    SamsunghealthPlugin updated to support the old plugin way

--- android/src/main/java/com/flutter/samsunghealth/SamsunghealthPlugin.java
+++ android/src/main/java/com/flutter/samsunghealth/SamsunghealthPlugin.java
@@ -1,50 +1,73 @@
 package com.flutter.samsunghealth;
 
+import android.app.Activity;
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * SamsunghealthPlugin
  */
 public final class SamsunghealthPlugin implements FlutterPlugin, ActivityAware {
 
+    private final static String CHANNEL = "plugins.flutter.io/samsunghealth";
     private MethodChannel methodChannel;
     private SamsungHealthModule samsungHealthModule;
 
+    public static void registerWith(final Registrar registrar) {
+        final SamsunghealthPlugin plugin = new SamsunghealthPlugin();
+        plugin.setUpChannel(registrar.context(), registrar.messenger());
+        plugin.attachToActivity(registrar.activity());
+    }
+
     @Override
     public final void onAttachedToEngine(@NonNull final FlutterPluginBinding flutterPluginBinding) {
-        methodChannel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "samsunghealth");
+        setUpChannel(flutterPluginBinding.getApplicationContext(), flutterPluginBinding.getBinaryMessenger());
     }
 
     @Override
     public final void onAttachedToActivity(final ActivityPluginBinding binding) {
-        samsungHealthModule = new SamsungHealthModule(binding.getActivity().getApplicationContext());
-        samsungHealthModule.onAttachToActivity(binding.getActivity());
-        methodChannel.setMethodCallHandler(samsungHealthModule);
+        attachToActivity(binding.getActivity());
     }
 
     @Override
     public final void onDetachedFromActivityForConfigChanges() {
-        samsungHealthModule.onDetachedFromActivity();
+        onDetachedFromActivity();
     }
 
     @Override
     public final void onReattachedToActivityForConfigChanges(final ActivityPluginBinding binding) {
-        samsungHealthModule.onAttachToActivity(binding.getActivity());
+        attachToActivity(binding.getActivity());
     }
 
     @Override
     public final void onDetachedFromEngine(@NonNull final FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
         samsungHealthModule.release();
     }
 
     @Override
     public final void onDetachedFromActivity() {
+        methodChannel.setMethodCallHandler(null);
         samsungHealthModule.release();
         samsungHealthModule.onDetachedFromActivity();
+    }
+
+    private void setUpChannel(final Context context, final BinaryMessenger messenger) {
+        samsungHealthModule = new SamsungHealthModule(context);
+        methodChannel = new MethodChannel(messenger, CHANNEL);
+        methodChannel.setMethodCallHandler(samsungHealthModule);
+    }
+
+    private void attachToActivity(final Activity activity) {
+        samsungHealthModule.onAttachToActivity(activity);
     }
 }
--- example/android/app/src/main/java/com/flutter/samsunghealth_example/MainActivity.java
+++ example/android/app/src/main/java/com/flutter/samsunghealth_example/MainActivity.java
@@ -1,19 +1,11 @@
 package com.flutter.samsunghealth_example;
 
-import android.os.Bundle;
-import android.view.View;
-
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-
-  @Override
-  public void setContentView(View view) {
-    super.setContentView(view);
-  }
 
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
